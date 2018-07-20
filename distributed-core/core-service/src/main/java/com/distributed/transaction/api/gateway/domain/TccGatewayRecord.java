487afb69157d3ceb23942934614f3e8e29e3969e
ssk <12606892+heheshang@users.noreply.github.com>
2018-07-20 12:55:41 +0800

    提交

--- distributed-core/core-service/src/main/java/com/distributed/transaction/api/gateway/domain/TccGatewayRecord.java
+++ distributed-core/core-service/src/main/java/com/distributed/transaction/api/gateway/domain/TccGatewayRecord.java
@@ -1,11 +1,18 @@
 package com.distributed.transaction.api.gateway.domain;
 
+import com.distributed.transaction.enums.convert.AreadlyDeadEnumConverter;
 import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 /**
  * 网关流水记录
@@ -20,5 +27,34 @@ import javax.persistence.Table;
 @Table(name = "tcc_gateway_record")
 @Data
 public class TccGatewayRecord {
+
+    /**
+     * 主键ID
+     */
+    @Id
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @GeneratedValue(generator = "system-uuid")
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "pay_key", nullable = false)
+    private String payKey;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "order_no", nullable = false)
+    private String orderNo;
+
+    @Column(name = "order_price", nullable = false)
+    private String orderPrice;
+
+    @Column(name = "pay_way_code", nullable = false)
+    @Convert(converter = AreadlyDeadEnumConverter.class)
+    private String payWayCode;
+
+    @Column(name = "order_ip")
+    private String orderIp;
+
 
 }
